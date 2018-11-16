bf73a38b3631fb16a47d6abd975ae8984fddb184
jyotiprakash2000 <42195507+jyotiprakash2000@users.noreply.github.com>
2018-11-16 09:42:40 +0530

    Update lab3_q2.cpp

--- lab3_q2.cpp
+++ lab3_q2.cpp
@@ -8,25 +8,7 @@ a=28;          //Defining the variable
 b=12;           //Defining the variable  
 sum=a+b;
 diff=a-b;
-product=a*b;
-using namespace std;
-int main()
-{
-int a,b,sum,diff,product,quotient,remainder;       //Declaring multiple variables
-a=28;          //Defining the variable
-b=12;           //Defining the variable  
-sum=a+b;
-diff=a-b;
-product=a*b;
-quotient=a/b;
-remainder=a%b;                                 //Using arithmetic operators
-cout <<"The sum of two numbers is " <<sum <<endl;
-cout <<"The difference of two numbers is " <<diff <<endl;
-cout <<"The product of two numbers is " <<product <<endl;
-cout <<"The quotient is " <<quotient <<endl; 
-cout <<"The remainder is " <<remainder <<endl;  //Printing out the results
-
-}
+product=a*b;                           
 quotient=a/b;
 remainder=a%b;                                 //Using arithmetic operators
 cout <<"The sum of two numbers is " <<sum <<endl;
